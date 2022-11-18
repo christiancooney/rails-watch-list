1c7374a3b8b049919c65848a9b3584f87fe128e0
christian cooney <christiancooney@hotmail.com>
2022-11-18 15:17:40 +0000

    Bundler fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'lists#index'
   resources :lists do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: %I[new create]
   end
   resources :bookmarks, only: [:destroy]
 end
